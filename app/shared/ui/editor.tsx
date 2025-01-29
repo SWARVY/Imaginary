3204482871bb5da91d0bc2c3cb80c5b57bd9017c
SWARVY <swarvy0826@naver.com>
2025-01-29 19:36:29 +0900

    feat: 에디터 초기화 로직 추가 및 `Zustand` 스토어 생성

--- app/shared/ui/editor.tsx
+++ app/shared/ui/editor.tsx
@@ -1,19 +1,22 @@
 import EditorJS from '@editorjs/editorjs';
 import { useEffect } from 'react';
+import { useEditorStore } from '~/store/editor-store';
 
 interface EditorComponentProps {
   className?: string;
 }
 
 export default function EditorComponent({ className }: EditorComponentProps) {
+  const { initialize } = useEditorStore();
+
   useEffect(() => {
     const editor = new EditorJS({
       holder: 'editorjs',
       tools: {},
       placeholder: '여기에 내용을 입력하세요...',
+      onReady: () => initialize(editor),
     });
 
-    // 컴포넌트 언마운트 시 에디터 정리
     return () => {
       editor.isReady
         .then(() => editor.destroy())
