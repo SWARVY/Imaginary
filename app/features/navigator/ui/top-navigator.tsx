9e5b0e0f102032162c540270f2afe169b6f704ef
SWARVY <swarvy0826@naver.com>
2025-01-30 00:48:26 +0900

    feat: 상단 내비게이터에서 로고 추가 및 홈 링크 간소화

--- app/features/navigator/ui/top-navigator.tsx
+++ app/features/navigator/ui/top-navigator.tsx
@@ -4,26 +4,9 @@ import { FaSearch } from 'react-icons/fa';
 export default function TopNavigator() {
   return (
     <nav className="navbar justify-between">
-      <div className="flex gap-x-2 p-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>
-        <Link
-          to="/new-post"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Write
-        </Link>
-      </div>
+      <h1 className="font-bold select-none">
+        <Link to="/">IMAGINARY</Link>
+      </h1>
       <UtilityButtons />
     </nav>
   );
