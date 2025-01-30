86e1da4427bfc9ab280140aabfeebe5886ec5179
SWARVY <swarvy0826@naver.com>
2025-01-31 00:58:48 +0900

    feat: `react-toastify`를 `sonner`로 변경 및 성공 메시지 수정

--- app/entities/post/model/useEditPost.ts
+++ app/entities/post/model/useEditPost.ts
@@ -2,7 +2,7 @@ import { useConvexMutation } from '@convex-dev/react-query';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from '@tanstack/react-router';
 import { api } from 'convex/_generated/api';
-import { toast } from 'react-toastify';
+import { toast } from 'sonner';
 
 export default function useEditPost() {
   const queryClient = useQueryClient();
@@ -16,7 +16,7 @@ export default function useEditPost() {
       queryClient.invalidateQueries({
         queryKey: [api.posts.getPostDetail, { id: variables.input._id }],
       });
-      toast.success('게시글이 수정되었습니다!');
+      toast.success('포스트가 수정되었어요 ✅');
       router.navigate({
         to: '/posts/list/$type',
         params: { type: 'POST' },
--- app/entities/post/model/useInsertPost.ts
+++ app/entities/post/model/useInsertPost.ts
@@ -2,7 +2,7 @@ import { useConvexMutation } from '@convex-dev/react-query';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from '@tanstack/react-router';
 import { api } from 'convex/_generated/api';
-import { toast } from 'react-toastify';
+import { toast } from 'sonner';
 
 export default function useInsertPost() {
   const router = useRouter();
@@ -11,7 +11,7 @@ export default function useInsertPost() {
     mutationKey: ['insert'],
     mutationFn: useConvexMutation(api.posts.createPost),
     onSuccess: () => {
-      toast.success('게시글이 등록되었습니다!');
+      toast.success('포스트가 등록되었어요 🚀');
       router.navigate({
         to: '/posts/list/$type',
         params: { type: 'POST' },
