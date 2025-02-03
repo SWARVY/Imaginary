d398c6fe6f3fb2015c296bc12dde002ff4151537
SWARVY <swarvy0826@naver.com>
2025-02-04 00:50:54 +0900

    feat: 포스트 상세 페이지에 데이터 로딩 및 메타 태그 추가

--- app/routes/posts/detail.$postId.tsx
+++ app/routes/posts/detail.$postId.tsx
@@ -1,17 +1,38 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { api } from 'convex/_generated/api';
 import { Id } from 'convex/_generated/dataModel';
+import { ConvexHttpClient } from 'convex/browser';
+import { seo } from '~/shared/lib/seo';
 import { PostViewer } from '~/widgets/post-viewer';
 
+const convexClient = new ConvexHttpClient(import.meta.env.VITE_CONVEX_URL!);
+
 export const Route = createFileRoute('/posts/detail/$postId')({
+  loader: async ({ params }) => {
+    const { postId } = params;
+    const data = await convexClient.query(api.posts.getPostDetail, {
+      id: postId as Id<'post'>,
+    });
+
+    return { data };
+  },
+  head: ({ loaderData }) => ({
+    meta: [
+      ...seo({
+        title: `Imaginary | ${loaderData?.data?.title}`,
+        description: `기술에 대한 깊이 있는 분석, 트러블슈팅 경험, 유용한 스니펫과 인사이트를 공유합니다.`,
+      }),
+    ],
+  }),
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { postId } = Route.useParams();
+  const { data } = Route.useLoaderData();
 
   return (
     <div className="size-full">
-      <PostViewer postId={postId as Id<'post'>} />
+      <PostViewer data={data} />
     </div>
   );
 }
